43603b4bbacdfc363f275c8bc9411a795513aebe
prjal-Bankoti <prajalbankoti971@gmail.com>
2021-12-30 22:28:58 +0530

    some changes

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
-const app = require("./src/index");
+const express = require("express");
+
+const userControllrs = require("./src/controllers/user.controller");
+const app = express();
+
+app.use(express.json());
+const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const helmet = require("helmet");
+app.use(cors());
+// app.use(bodyParser.json());
+
+// app.use(helmet());
+// app.use(multer().any());
+app.use(express.static("public"));
+app.use(express.static(__dirname));
+app.use("/uploads", express.static("uploads"));
+
+app.use("/user", userControllrs);
+module.exports = app;
+
 require("dotenv").config();
 
 const connect = require("./src/configs/db");
