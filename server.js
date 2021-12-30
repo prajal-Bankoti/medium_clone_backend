398efed3d308cea006e88cb3a496400d5230befc
prjal-Bankoti <prajalbankoti971@gmail.com>
2021-12-30 22:34:52 +0530

    some changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require("dotenv").config();
 
 const connect = require("./src/configs/db");
 let port = process.env.Port || 7000;
-app.listen(port, () => {
-  connect();
+app.listen(port,async () => {
+ await connect();
   console.log("listing port " + port);
 });
